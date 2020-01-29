e4714d1c54718e18228a4cea2d54d33f80d23613
Steve Calvert <scalvert@linkedin.com>
2020-01-28 16:26:00 -0800

    task(qunit-fixtures): Integration with qunit-fixtures package

--- packages/console-test-helpers/src/index.ts
+++ packages/console-test-helpers/src/index.ts
@@ -1,5 +1,4 @@
 export { default as mockConsole } from './mock-console';
-export { default as FixtureCache } from './fixture-cache';
 export { default as ConsoleState } from './console-state';
 
 export * from './types';
